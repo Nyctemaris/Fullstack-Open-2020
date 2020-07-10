e551adb0d875d69c42dbaa12e987387d824a0021
Nyctemaris <allu94@hotmail.com>
2020-07-10 15:18:59 +0300

    Implement buttons and statistics

--- Osa 1/unicafe/src/index.js
+++ Osa 1/unicafe/src/index.js
@@ -1,17 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, {useState} from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const Button = ({text, handleClick}) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
 );
+const Statistics = (props) => (
+  <div>
+    <StatisticsLine text='Good' value={props.good} />
+    <StatisticsLine text='Neutral' value={props.neutral} />
+    <StatisticsLine text='Bad' value={props.bad} />
+  </div>
+);
+const StatisticsLine = ({text, value}) => (
+  <div>{text} {value}</div>
+);
+const App = () => {
+  // tallenna napit omaan tilaansa
+  const header = 'Give Feedback'
+  const statistics = 'statistics'
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  return (
+    <div>
+      <div>
+        {header}
+      </div>
+      <div>
+        <Button text='Good' handleClick={() => setGood(good + 1)} />
+        <Button text='Neutral' handleClick={() => setNeutral(neutral + 1)}/>
+        <Button text='Bad' handleClick={() => setBad(bad + 1)}/>
+      </div>
+      <div>
+        {statistics}
+      </div>
+      <Statistics good={good} neutral={neutral} bad={bad}/>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />,
+  document.getElementById('root')
+)
