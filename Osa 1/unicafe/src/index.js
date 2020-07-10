baa018848629942e908dc49f5d1a6754716696b4
Nyctemaris <allu94@hotmail.com>
2020-07-10 15:47:13 +0300

    Implement unicafe step4

--- Osa 1/unicafe/src/index.js
+++ Osa 1/unicafe/src/index.js
@@ -23,15 +23,32 @@ const getPositive = (props) => {
   )
 }
 const Statistics = (props) => {
-
+  let statisticsContent =
+    <div>
+      <h4>
+        Statistics
+      </h4>
+      <h6>
+        No feedback given
+      </h6>
+    </div>;
+  if (getAll(props) !== 0) {
+    statisticsContent =
+      <div>
+        <h4>
+          Statistics
+        </h4>
+        <StatisticsLine text='Good' value={props.good} />
+        <StatisticsLine text='Neutral' value={props.neutral} />
+        <StatisticsLine text='Bad' value={props.bad} />
+        <StatisticsLine text='All' value={getAll(props)} />
+        <StatisticsLine text='Average' value={getAverage(props)} />
+        <StatisticsLine text='Positive' value={getPositive(props) + '%'} />
+      </div>;
+  }
   return (
     <div>
-      <StatisticsLine text='Good' value={props.good} />
-      <StatisticsLine text='Neutral' value={props.neutral} />
-      <StatisticsLine text='Bad' value={props.bad} />
-      <StatisticsLine text='All' value={getAll(props)} />
-      <StatisticsLine text='Average' value={getAverage(props)} />
-      <StatisticsLine text='Positive' value={getPositive(props) + '%'} />
+      {statisticsContent}
     </div>)
 }
 const StatisticsLine = ({ text, value }) => (
@@ -40,23 +57,19 @@ const StatisticsLine = ({ text, value }) => (
 const App = () => {
   // tallenna napit omaan tilaansa
   const header = 'Give Feedback'
-  const statistics = 'statistics'
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
   return (
     <div>
-      <div>
+      <h2>
         {header}
-      </div>
+      </h2>
       <div>
         <Button text='Good' handleClick={() => setGood(good + 1)} />
         <Button text='Neutral' handleClick={() => setNeutral(neutral + 1)} />
         <Button text='Bad' handleClick={() => setBad(bad + 1)} />
-      </div>
-      <div>
-        {statistics}
       </div>
       <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
