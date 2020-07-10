acfde6b56c0ea6fecc8e8b49d97f1dbc509fbeda
Nyctemaris <allu94@hotmail.com>
2020-07-10 16:36:46 +0300

    Implement anekdootit step2

--- Osa 1/anekdootit/src/index.js
+++ Osa 1/anekdootit/src/index.js
@@ -1,17 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Button = ({ text, handleClick }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+);
+const randomAnecdote = (selected) => {
+  // Generate random number until it does not match the current anecdote number
+  // We don't want to show the same anecdote twice in row
+
+  let randomNumber;
+  do {
+    randomNumber = Math.floor(Math.random() * Math.floor(anecdotes.length));
+  } while (selected === randomNumber)
+  return (
+    randomNumber
+  )
+}
+const handleVote = (selected, votes) => {
+  let votesTemp = [...votes];
+  votesTemp[selected] += 1;
+  return (
+    votesTemp
+  )
+}
+const App = (props) => {
+  const [selected, setSelected] = useState(0)
+
+  // Create a new table that has size equal to anecdote amount and fill it with 0's
+  const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0));
+
+  return (
+    <div>
+      <div>
+        {props.anecdotes[selected]}
+      </div>
+      <div>
+        {votes[selected]}
+      </div>
+      <div>
+        <Button text='vote' handleClick={() => setVotes(handleVote(selected, votes))} />
+        <Button text='next anecdote' handleClick={() => setSelected(randomAnecdote(selected))} />
+      </div>
+    </div>
+  )
+}
+new Array(6).fill(0);
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <App anecdotes={anecdotes} />,
   document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+)
