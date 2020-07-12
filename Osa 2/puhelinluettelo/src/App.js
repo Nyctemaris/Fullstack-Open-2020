b06a031961650a551f1423f6d74036bb04a6d3d3
Nyctemaris <allu94@hotmail.com>
2020-07-12 16:57:45 +0300

    Implement phonebook rendering and number field

--- Osa 2/puhelinluettelo/src/App.js
+++ Osa 2/puhelinluettelo/src/App.js
@@ -3,29 +3,49 @@ import './App.css';
 //import './components/NewPersonForm.js'
 
 const App = () => {
+
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const addPerson = (event) => {
     event.preventDefault()
 
+    if (persons.find(person => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`)
+      return;
+    }
+
     const nameObject = {
       name: newName,
+      number: newNumber,
       id: persons.length + 1
     }
+
     setPersons(persons.concat(nameObject))
     setNewName('')
+
   }
-  const handleFieldChange = (event) => {
+
+  const handleNameFieldChange = (event) => {
     console.log(event.target.value)
     setNewName(event.target.value)
   }
+
+  const handleNumberFieldChange = (event) => {
+    console.log(event.target.value)
+    setNewNumber(event.target.value)
+  }
+
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={addPerson}>
         <div>
-          name: <input value={newName} onChange={handleFieldChange} />
+          name: <input value={newName} onChange={handleNameFieldChange} />
+        </div>
+        <div>
+          number: <input value={newNumber} onChange={handleNumberFieldChange}/>
         </div>
         <div>
           <button type="submit">add</button>
@@ -33,7 +53,7 @@ const App = () => {
       </form>
       <div>debug: {newName}</div>
       <h2>Numbers</h2>
-      <div>{persons.map((nameObject) => (<p key={nameObject.id}>{nameObject.name}</p>))}</div>
+      <div>{persons.map((nameObject) => (<p key={nameObject.id}>{nameObject.name} {nameObject.number}</p>))}</div>
     </div>
   )
 
