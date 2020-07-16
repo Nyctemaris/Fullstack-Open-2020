cdcc9f14f2f6ca5766bb5277f1ebf47d0813cdb2
Nyctemaris <allu94@hotmail.com>
2020-07-16 17:24:35 +0300

    baseURL address fix

--- Osa 2/puhelinluettelo/src/Services/dataService.js
+++ Osa 2/puhelinluettelo/src/Services/dataService.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseURL = 'http://localhost:3001/persons'
+const baseURL = 'http://localhost:3001/api/persons'
 
 const fetchAllData = async () => {
     const request = axios.get(baseURL)
